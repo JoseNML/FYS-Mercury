d98d13cbb55187ccb9c610e9514613767c15d162
Mitchell7112 <Mitchellwan28@gmail.com>
2017-11-13 19:02:50 +0100

    Action broken

--- Mercury/src/main/java/hva/fys/mercury/BagageZoekenController.java
+++ Mercury/src/main/java/hva/fys/mercury/BagageZoekenController.java
@@ -5,17 +5,22 @@ package hva.fys.mercury;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 
-/**
- * FXML Controller class
- *
- * @author davidbritt
- */
 public class BagageZoekenController implements Initializable {
+
+    @FXML
+    private void zoekAction(ActionEvent event) {
+        System.out.println("Opening search results");
+        Parent pane = loadFXMLFile("/fxml/bagageResultaten.fxml");
+    }
 
     /**
      * Initializes the controller class.
@@ -23,6 +28,15 @@ public class BagageZoekenController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+
+    }
     
+        private Parent loadFXMLFile(String fxmlFileName) {
+        try {
+            return FXMLLoader.load(getClass().getResource(fxmlFileName));
+        } catch (IOException ex) {
+            System.out.printf("\n%s: %s\n", ex.getClass().getName(), ex.getMessage());
+            return null;
+        }
+    }
 }
